a4e1b7983c1aad432e682e3a9cfbdb16361830ae
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-06 07:56:05 -0700

    setup root controller

---
+++ ios_rotten_tomatoes/MovieListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieListViewController.h
+//  ios_rotten_tomatoes
+//
+//  Created by Stanley Ng on 6/6/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieListViewController : UIViewController
+
+@end
---
+++ ios_rotten_tomatoes/MovieListViewController.m
@@ -0,0 +1,38 @@
+//
+//  MovieListViewController.m
+//  ios_rotten_tomatoes
+//
+//  Created by Stanley Ng on 6/6/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import "MovieListViewController.h"
+
+@interface MovieListViewController ()
+
+@end
+
+@implementation MovieListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
