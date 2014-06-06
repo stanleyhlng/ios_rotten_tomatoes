ec9cf89a34496b0c154c91b415330b9f36a3ce2a
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-06 07:40:24 -0700

    Initial Commit

---
+++ ios_rotten_tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_rotten_tomatoes
+//
+//  Created by Stanley Ng on 6/6/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_rotten_tomatoesTests/ios_rotten_tomatoesTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_rotten_tomatoesTests.m
+//  ios_rotten_tomatoesTests
+//
+//  Created by Stanley Ng on 6/6/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_rotten_tomatoesTests : XCTestCase
+
+@end
+
+@implementation ios_rotten_tomatoesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
